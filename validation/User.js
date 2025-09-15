30543ec74f3c85b1c47a76267e73788a254c26a2
JulienCASTE <castejon.julien@gmail.com>
2025-09-15 19:22:20 +0200

    Move unique constraints in routes instead of validation

--- validation/User.js
+++ validation/User.js
@@ -1,6 +1,5 @@
 const { body } = require("express-validator");
 const handleValidationErrors = require("../middlewares/handleValidationErrors");
-const User = require("../models/User");
 
 const userSignupValidators = [
   body("email")
@@ -8,14 +7,7 @@ const userSignupValidators = [
     .notEmpty()
     .isEmail()
     .withMessage("Email invalide")
-    .normalizeEmail()
-    .custom(async (email, { req }) => {
-      const existing = await User.findOne({ email });
-      if (existing) {
-        throw new Error("Cet email est déjà utilisé");
-      }
-      return true;
-    }),
+    .normalizeEmail(),
   body("firstname")
     .trim()
     .notEmpty()
@@ -31,14 +23,7 @@ const userSignupValidators = [
     .trim()
     .notEmpty()
     .isLength({ min: 2, max: 30 })
-    .withMessage("Le pseudo doit faire entre 2 et 30 caractères")
-    .custom(async (nickname, { req }) => {
-      const existing = await User.findOne({ nickname });
-      if (existing) {
-        throw new Error("Ce pseudo est déjà utilisé");
-      }
-      return true;
-    }),
+    .withMessage("Le pseudo doit faire entre 2 et 30 caractères"),
   body("password")
     .notEmpty()
     .isLength({ min: 6 })
@@ -63,14 +48,7 @@ const userUpdateValidators = [
     .notEmpty()
     .isEmail()
     .withMessage("Email invalide")
-    .normalizeEmail()
-    .custom(async (email, { req }) => {
-      const existing = await User.findOne({ email });
-      if (existing && !existing._id.equals(req.user._id)) {
-        throw new Error("Cet email est déjà utilisé woulah");
-      }
-      return true;
-    }),
+    .normalizeEmail(),
   body("firstname")
     .trim()
     .notEmpty()
@@ -86,14 +64,7 @@ const userUpdateValidators = [
     .trim()
     .notEmpty()
     .isLength({ min: 2, max: 30 })
-    .withMessage("Le pseudo doit faire entre 2 et 30 caractères")
-    .custom(async (nickname, { req }) => {
-      const existing = await User.findOne({ nickname });
-      if (existing && !existing._id.equals(req.user._id)) {
-        throw new Error("Ce pseudo est déjà utilisé woulah");
-      }
-      return true;
-    }),
+    .withMessage("Le pseudo doit faire entre 2 et 30 caractères"),
   handleValidationErrors,
 ];
 
