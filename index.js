cc59d7836cc4c122e8439cc7c6aaa1e0fc6629b2
tomlrd <thomas.laroudie@gmail.com>
2025-09-16 11:51:10 +0200

    fix dotenv

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-dotenv.config();
 const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary").v2;
